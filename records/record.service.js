bf8a1310fcb9527358f9e09f027811c32a3132d1
kiranbandi <bvenkatkiran@gmail.com>
2020-01-17 14:16:14 -0600

    fixed breaking code

--- records/record.service.js
+++ records/record.service.js
@@ -5,7 +5,6 @@ module.exports = {
     getRecordByUserName,
     createMultiple,
     deleteRecords,
-    deleteUGRecords,
     getAllRecords,
     getAllObserversList,
     getRecordsByObserverName
